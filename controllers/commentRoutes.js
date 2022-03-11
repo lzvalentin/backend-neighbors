3f932d54da387d357f593c0a68ebd00f57c1cce9
Bernardo Dibildox <95992081+bdibil@users.noreply.github.com>
2022-03-11 11:42:29 -0800

    added tokenAuth middelware and changes to user and post routes

--- controllers/commentRoutes.js
+++ controllers/commentRoutes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
-const comment = require ('../models/Comment')
+// const Comment = require ('../models/Comment')
+const { User, Comment } = require("../models");
 
 
 
 // Get comments
 router.get('/', (req, res) => {
-  Comment.findAll({})
+  Comment.findAll({
+    include: [User]
+  })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
       console.log(err);
@@ -13,24 +16,57 @@ router.get('/', (req, res) => {
     });
 }); 
 
-//create a comment
 
+//create a comment
+router.post('/', async (req, res) => {
+  try {
+    const commentData = await Comment.create(req.body);
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
 // GET single comment by id
 router.get('/:id', async (req, res) => {
   try {
-    const commentData = await comment.findByPk(req.params.id);
+    const commentData = await Comment.findByPk(req.params.id);
     if (!commentData) {
       res.status(404).json({ message: 'No comment with this id!' });
       return;
     }
-    res.status(200).json(userData);
+    res.status(200).json(commentData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+
+
+// GET ALL comments from a user
+router.get('/usercomment/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      include: [{
+        model: User,
+        where: {id:req.params.id}
+      }]
+
+    })
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment with this id!' });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 
 
 module.exports = router;
