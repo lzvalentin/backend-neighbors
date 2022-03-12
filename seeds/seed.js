0bf87af9cd6de6cd3dbf1f1da590cad73a53c0f7
Bernardo Dibildox <95992081+bdibil@users.noreply.github.com>
2022-03-12 14:20:07 -0800

    isAdmin on Users Model

--- seeds/seed.js
+++ seeds/seed.js
@@ -14,6 +14,7 @@ const seed = async ()=>{
             email:"joe@joe.joe",
             password:"password",
             address: "123 Joe St. Seattle, WA",
+            isAdmin: true,
             profile_pic_url: "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d",
         },
         {
